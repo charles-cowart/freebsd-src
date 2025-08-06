d2b5ae589a636038873bf3b4da24c59418a2f773
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2025-08-06 15:38:58 -0700

    Remove obsolete communication utilities

--- usr.bin/login/pathnames.h
+++ usr.bin/login/pathnames.h
@@ -32,6 +32,5 @@
 #include <paths.h>
 
 #define	_PATH_HUSHLOGIN		".hushlogin"
-#define	_PATH_MOTDFILE		"/var/run/motd"
 #define _PATH_FBTAB		"/etc/fbtab"
 #define _PATH_LOGINDEVPERM	"/etc/logindevperm"
