4021b1d7fb3749987ad7962446eb35eff121b48c
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2026-09-19 16:33:32 -0700

    ofed: remove osmcomp

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -229,8 +229,7 @@ _LIBRARIES+= \
 		mlx4 \
 		mlx5 \
 		bnxtre \
-		rdmacm \
-		osmcomp 
+		rdmacm
 .endif
 
 .if ${MK_BEARSSL} == "yes"
@@ -410,7 +409,6 @@ _DP_mlx4=	ibverbs pthread
 _DP_mlx5=	ibverbs pthread
 _DP_bnxtre=	ibverbs pthread
 _DP_rdmacm=	ibverbs
-_DP_osmcomp=	pthread
 .endif
 
 # Define special cases
@@ -670,7 +668,6 @@ LIBMLX4DIR=	${_LIB_OBJTOP}/lib/ofed/libmlx4
 LIBMLX5DIR=	${_LIB_OBJTOP}/lib/ofed/libmlx5
 LIBBNXTREDIR=	${_LIB_OBJTOP}/lib/ofed/libbnxtre
 LIBRDMACMDIR=	${_LIB_OBJTOP}/lib/ofed/librdmacm
-LIBOSMCOMPDIR=	${_LIB_OBJTOP}/lib/ofed/complib
 LIBSSPDIR=	${_LIB_OBJTOP}/lib/libssp
 LIBSSP_NONSHAREDDIR=	${_LIB_OBJTOP}/lib/libssp_nonshared
 LIBATF_CDIR=	${_LIB_OBJTOP}/lib/atf/libatf-c
