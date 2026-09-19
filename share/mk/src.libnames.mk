4871e901bb229afbc15b88a40d80f59a062c0799
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2026-09-19 16:33:44 -0700

    ofed: remove libibmad

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -222,7 +222,6 @@ _LIBRARIES+= \
 _LIBRARIES+= \
 		cxgb4 \
 		ibcm \
-		ibmad \
 		ibumad \
 		ibverbs \
 		irdma \
@@ -402,7 +401,6 @@ _DP_iscsiutil=	md
 .if ${MK_OFED} != "no"
 _DP_cxgb4=	ibverbs pthread
 _DP_ibcm=	ibverbs
-_DP_ibmad=	ibumad
 _DP_ibumad=
 _DP_ibverbs=
 _DP_irdma=	ibverbs pthread
@@ -662,7 +660,6 @@ LIBTPOOLDIR=	${_LIB_OBJTOP}/cddl/lib/libtpool
 # OFED support
 LIBCXGB4DIR=	${_LIB_OBJTOP}/lib/ofed/libcxgb4
 LIBIBCMDIR=	${_LIB_OBJTOP}/lib/ofed/libibcm
-LIBIBMADDIR=	${_LIB_OBJTOP}/lib/ofed/libibmad
 LIBIBUMADDIR=	${_LIB_OBJTOP}/lib/ofed/libibumad
 LIBIBVERBSDIR=	${_LIB_OBJTOP}/lib/ofed/libibverbs
 LIBIRDMADIR=	${_LIB_OBJTOP}/lib/ofed/libirdma
